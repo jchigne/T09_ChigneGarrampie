e3437e99566a9ed6154dfb45a27fc87f68a07afd
Chigne Garrampie <jchigne@unprg.edu.pe>
2019-12-18 01:06:27 -0500

    ejercicio22

---
+++ ChigneGarrampie/funciones22.py
@@ -0,0 +1,20 @@
+# Ejercicio 16
+import os
+import libreria
+
+# Calcular la aceleracion angular
+# Declaracion de datos
+
+#imput
+v_angular=int(os.sys.argv[1])
+radio=int(os.sys.argv[2])
+a_angular=libreria.a_angular(v_angular,radio)
+
+if (a_angular>=100):                                           # Si la aceleracion angular supera los 100
+    print("El movimiento es veloz")
+else:                                                         # Si la aceleracion angular no supera los 1000
+    print("El movimiento  no es veloz")
+#fin_if
+
+#output
+print("El volumen de la picina es:",a_angular)
